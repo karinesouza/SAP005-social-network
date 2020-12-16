44c79ad79b4a575d798686d092bb8d065136c242
KarinaFS <karina_fsantos@outlook.com>
2020-12-16 18:56:29 -0300

    Adicinado campo de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,7 +2,50 @@ export const Login = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
-      <h1>Login!</h1>
+    <div class="form-login" 
+     <h1 class="title">PetLovers</h1>
+      <form id="login">
+        <h2>Acesse sua conta</h2>
+        <label class="e-mail">E-mail</label> 
+        <input class="e-mail" type="e-mail" placeholder="Digite o e-mail cadastrado" required>
+        <label class="senha">Senha</label>
+        <input class="senha" type="password" placeholder="Digite sua senha" required>
+        <input type="button" id="buttonlogin" value="Entrar">
+        <p>Não tem cadastro? <a href="">Registre-se</a></p>
+      </form>
+    </div> 
   `;
   return rootElement;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

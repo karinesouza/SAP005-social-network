d2b730d0fea321acbde03dc234bbccb75ec8ad5f
KarinaFS <karina_fsantos@outlook.com>
2020-12-17 18:00:56 -0300

    Registrar-se

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,21 +2,25 @@ export const Login = () => {
   // Coloque sua página
   const rootElement = document.createElement('div');
   rootElement.innerHTML = `
-    <div class="form-login" 
-     <h1 class="title">PetLovers</h1>
+    <div class="login-form" 
       <form id="login">
+        <h1 class="title">PetLovers</h1>
         <h2>Acesse sua conta</h2>
         <label class="e-mail">E-mail</label> 
         <input class="e-mail" type="e-mail" placeholder="Digite o e-mail cadastrado" required>
-        <label class="senha">Senha</label>
-        <input class="senha" type="password" placeholder="Digite sua senha" required>
-        <input type="button" id="buttonlogin" value="Entrar">
-        <p>Não tem cadastro? <a href="">Registre-se</a></p>
+        <label class="password">Senha</label>
+        <input class="password" type="password" placeholder="Digite sua senha" required>
+        <input type="button" id="btn-login" value="Entrar">
+        <p>Não tem cadastro? <a class="items" href="#register-form" id="btn-log"> Cadastre-se</a>
       </form>
     </div> 
   `;
   return rootElement;
 };
+
+
+
+
 
 
 
--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,41 @@
-// exporte suas funções
+// // exporte suas funções
 
-export const myFunction = () => {
-  // seu código aqui
-  console.log('Olá mundo!');
-};
+// export const myFunction = () => {
+//   // seu código aqui
+//   console.log('Olá mundo!');
+// };
+
+
+// LOGIN DE NOVOS USUÁRIOS
+const email = "teste@teste.com" //pegar do input email
+const password = "banana"       //pegar do input senha
+
+firebase
+  .auth()
+  .createUserWithEmailAndPassword(email, password)
+  .then(() => console.log("deu certo")) 
+  .catch((error) => {
+    // Signed in
+  
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    console.log("deu ruim")
+    // ..
+  });
+
+  // LOGIN DE USUÁRIOS EXISTENTES
+
+firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((user) => {
+    // Signed in
+    // ...
+  })
+  .catch((error) => {
+    let errorCode = error.code;
+    let errorMessage = error.message;
+  });
+
+  // AUTENTICAÇÃO COM GOOGLE
+
+let provider = new firebase.auth.GoogleAuthProvider();  
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
